ca6f61a516eb66e147974f6d8ea08cb7d86a1d32
Luís Felipe <luis.felipeszsv@gmail.com>
2021-11-26 10:58:15 -0300

    creating user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  id: string;
+  accountCode: string;
+  cpf: string;
+  email: string;
+  phone: string;
+  balance: number;
+  extract: number[];
+  created_at: Date;
+  updated_at: Date;
+}
